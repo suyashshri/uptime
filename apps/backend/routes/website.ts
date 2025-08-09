d5992cac05d5f631971894a116841306c382f1f6
suyashshri <suyashshrivastava736@gmail.com>
2025-08-09 21:27:52 +0530

    updated all website endpoints

--- apps/backend/routes/website.ts
+++ apps/backend/routes/website.ts
@@ -6,42 +6,100 @@ import { authmiddleware } from "../middleware/authmiddleware";
 const router = Router();
 
 router.post("/create-website", authmiddleware, async (req, res) => {
-  const data = req.body;
-  const parsedData = CreateWebsiteSchema.safeParse(data);
+  try {
+    const data = req.body;
+    const parsedData = CreateWebsiteSchema.safeParse(data);
 
-  if (!parsedData.success) {
-    res.status(400).json({
-      message: "Invalid/Incorrect Inputs provided",
+    if (!parsedData.success) {
+      res.status(400).json({
+        message: "Invalid/Incorrect Inputs provided",
+      });
+    }
+
+    const { name, url } = parsedData.data!;
+
+    const website = await prisma.website.create({
+      data: {
+        name,
+        url,
+        userId: req.userId!,
+      },
     });
+    res.status(201).json({
+      message: "Website Created successfully",
+      id: website.id,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "Error creating website",
+    });
+    return;
   }
-
-  const { name, url } = parsedData.data!;
-
-  const website = await prisma.website.create({
-    data: {
-      name,
-      url,
-      userId: req.userId!,
-    },
-  });
-  res.status(201).json({
-    message: "Website Created successfully",
-    id: website.id,
-  });
 });
 
 router.post("/status/:websiteId", authmiddleware, async (req, res) => {
-  const websiteId = req.params.websiteId;
+  try {
+    const websiteId = req.params.websiteId;
 
-  const website = await prisma.website.findUnique({
-    where: {
-      id: websiteId,
-      userId: req.userId,
-    },
-  });
+    const website = await prisma.website.findUnique({
+      where: {
+        id: websiteId,
+        userId: req.userId,
+      },
+      include: {
+        WebsiteTicks: {
+          orderBy: {
+            createAt: "desc",
+          },
+        },
+      },
+    });
 
-  if (!website) {
-    res.status(404).json({ message: "Website not found" });
+    if (!website) {
+      res.status(404).json({ message: "Website not found" });
+      return;
+    }
+
+    res.status(201).json({ message: "Website Status", website });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "Error finding status of this website",
+    });
+    return;
+  }
+});
+
+router.post("/status/all", authmiddleware, async (req, res) => {
+  try {
+    const websites = await prisma.website.findMany({
+      where: {
+        userId: req.userId,
+      },
+      include: {
+        WebsiteTicks: {
+          orderBy: [
+            {
+              createAt: "desc",
+            },
+          ],
+          take: 1,
+        },
+      },
+    });
+    if (!websites) {
+      res.status(404).json({ message: "Website not found" });
+      return;
+    }
+    res.status(201).json({ message: "Website Status", websites });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "Error finding status of all website",
+    });
     return;
   }
 });
---
+++ packages/db/prisma/migrations/20250809154753_updated_websiteticks_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."WebsiteTicks" ADD COLUMN     "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
